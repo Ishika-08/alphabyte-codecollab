1b87639a0121cc0fb1027923ce5e047ac1fb1e1f
mohit pardeshi <mohitpardeshi729@gmail.com>
2024-03-20 10:50:45 +0530

    server cors policy changed

--- server/index.js
+++ server/index.js
@@ -20,6 +20,12 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  next() 
+  })
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 const EditorRoutes = require('./routes/EditorRoutes');
